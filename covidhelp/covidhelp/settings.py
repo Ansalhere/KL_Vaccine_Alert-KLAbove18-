bd606e7b6c9facda4c5772b1e8106ce7bdee31f8
Ansalhere <49818863+Ansalhere@users.noreply.github.com>
2021-06-10 00:13:45 +0530

    Update settings.py

--- covidhelp/covidhelp/settings.py
+++ covidhelp/covidhelp/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sdbh-m45v18kcuf7+wc^@m_x=%u$09q%7109m80%jp1g#c&$3@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
