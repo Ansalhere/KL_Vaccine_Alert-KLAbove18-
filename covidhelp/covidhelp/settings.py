973f7623a05771ae01b31a9b7847e3bc356d7d2e
Ansalhere <49818863+Ansalhere@users.noreply.github.com>
2021-07-07 00:38:20 +0530

    Update settings.py

--- covidhelp/covidhelp/settings.py
+++ covidhelp/covidhelp/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [
+STATIC_ROOT = [
     os.path.join(BASE_DIR, 'vaccinhelp/static')
 ]
 STATIC_URL = '/static/'
