67a89989b8cdad68a827e4082e7392bf613e67ad
Ansalhere <49818863+Ansalhere@users.noreply.github.com>
2021-07-23 20:38:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from covidhelp.vaccinhelp.views import fetch_data_from_cowin
+from covidhelp.vaccinhelp.views import *
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    fetch_data_from_cowin(307)
+    districts_ids =  (307,301,306,297,295,298,304,305,302,308,300,296,303,299)
+    pincodes = (683556,)
+    for x in districts_ids:
+        dist_fetch_data_from_cowin(x)
+    for y in pincodes:
+        fetch_data_from_cowin(y)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
